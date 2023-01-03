5d0d4d0c12fab7039bc56d4e27a5bfbf6ed39746
Natreve <natreve.ag20@gmail.com>
2023-01-03 11:29:14 -0400

    updated bot response code to allow for it to answer questions directed at it when added to a group

--- functions/index.js
+++ functions/index.js
@@ -9,25 +9,44 @@ dotenv.config();
 const app = express();
 const bot = new Bot(process.env.TELEGRAM_BOT_API);
 const config = new Configuration({ apiKey: process.env.OPENAI_API });
-
 const openai = new OpenAIApi(config);
+
 app.use(express.json());
 app.use(webhookCallback(bot));
 
-bot.command("start", (ctx) => {
-  ctx.reply("Hello, Welcome to Codesby's OpenAI Telegram bot!");
-});
-
-bot.on("message:text", async (ctx) => {
-  const { text, from } = ctx.message;
-
+async function gtpRespond(text) {
   const result = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: text,
     temperature: 0,
     max_tokens: 1000,
   });
+  return result.data.choices[0].text;
+}
 
-  ctx.reply(result.data.choices[0].text);
+bot.command("start", (ctx) => {
+  ctx.reply("🤖");
+  ctx.reply("Hello, Welcome to Codesby's OpenAI Telegram bot!");
+});
+
+bot.on("message:text", async (ctx) => {
+  const { text } = ctx.message;
+  const me = `@${bot.botInfo.username}`;
+  const regex = new RegExp(`${me}`, "i");
+
+  if (ctx.chat.type === "private") {
+    await ctx.reply("🤖");
+    ctx.replyWithChatAction("typing");
+    const response = await gtpRespond(text);
+    await ctx.reply(response);
+    return;
+  }
+  if (!regex.test(text)) return;
+  await ctx.reply("🤖");
+  ctx.replyWithChatAction("typing");
+  const response = await gtpRespond(text);
+  await ctx.reply(response, {
+    reply_to_message_id: ctx.msg.message_id,
+  });
 });
 export const CodesbyGPT3Bot = functions.https.onRequest(app);
